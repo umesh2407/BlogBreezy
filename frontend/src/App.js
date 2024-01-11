80e818e5c213a4a7b18c9a56d53a45cb45cf1f4e
umesh2407 <choudharyumesh986@gmail.com>
2024-01-11 23:26:21 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+   <main>
+    <header>
+      <a href="" className="logo">MyBlog</a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+
+  <div className="post">
+    <div className="image">
+    <img src="https://techcrunch.com/wp-content/uploads/2022/08/GettyImages-1042683346.jpg?w=850&h=492&crop=1"></img>
+    </div>
+    <div className="texts">
+    <h2>Is India done with crypto?</h2>
+    <p>It wasn’t long ago when Indian venture capitalists were scrambling to establish their crypto credentials. ENS addresses adorned Twitter profiles.</p>
+    </div>
+  </div>
+  <div className="post">
+  <div className="image">
+    <img src="https://techcrunch.com/wp-content/uploads/2022/08/GettyImages-1042683346.jpg?w=850&h=492&crop=1"></img>
+    </div>
+    <div className="texts">
+    <h2>Is India done with crypto?</h2>
+    <p>It wasn’t long ago when Indian venture capitalists were scrambling to establish their crypto credentials. ENS addresses adorned Twitter profiles.</p>
+    </div>
+  </div>
+  <div className="post">
+  <div className="image">
+    <img src="https://techcrunch.com/wp-content/uploads/2022/08/GettyImages-1042683346.jpg?w=850&h=492&crop=1"></img>
+    </div>
+    <div className="texts">
+    <h2>Is India done with crypto?</h2>
+    <p>It wasn’t long ago when Indian venture capitalists were scrambling to establish their crypto credentials. ENS addresses adorned Twitter profiles.</p>
+    </div>
+  </div>
+   </main>
+  );
+}
+
+export default App;
