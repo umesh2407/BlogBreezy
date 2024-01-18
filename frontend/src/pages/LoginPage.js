a2c37a55c7f0ee2078b799157efd247bfc77cb9e
umesh2407 <choudharyumesh986@gmail.com>
2024-01-18 23:55:39 +0530

    edited login/register

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,6 +2,7 @@
 const LoginPage = () => {
   return (
    <form className="login">
+    <h1>Login</h1>
      <input type="text" placeholder="username"/>
      <input type="password" placeholder="password"/>
      <button>Login</button>
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -2,6 +2,7 @@
 const RegisterPage = () => {
   return (
     <form className="register">
+      <h1>Register</h1>
      <input type="text" placeholder="username"/>
      <input type="password" placeholder="password"/>
      <button>Register</button>
