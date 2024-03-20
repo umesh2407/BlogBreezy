aa81c9ea7f09e81611373fd61bd33d14322439f8
umesh2407 <choudharyumesh986@gmail.com>
2024-03-20 21:21:53 +0530

    completed project

--- backend/index.js
+++ backend/index.js
@@ -93,6 +93,70 @@ jwt.verify(token, secret, {}, async(err,info)=>{
 })
 })
 
+// app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+//     let newPath = null;
+//     if (req.file) {
+//       const {originalname,path} = req.file;
+//       const parts = originalname.split('.');
+//       const ext = parts[parts.length - 1];
+//       newPath = path+'.'+ext;
+//       fs.renameSync(path, newPath);
+//     }
+  
+//     const {token} = req.cookies;
+//     jwt.verify(token, secret, {}, async (err,info) => {
+//       if (err) throw err;
+//       const {id,title,summary,content} = req.body;
+//       const postDoc = await Post.findById(id);
+//       const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+//       if (!isAuthor) {
+//         return res.status(400).json('you are not the author');
+//       }
+//       await postDoc.update({
+//         title,
+//         summary,
+//         content,
+//         cover: newPath ? newPath : postDoc.cover,
+//       });
+  
+//       res.json(postDoc);
+//     });
+  
+//   });
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('you are not the author');
+        }
+
+        // Replace `update` with `findByIdAndUpdate`
+        const updatedPost = await Post.findByIdAndUpdate(id, {
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        }, { new: true }); // to return the updated document
+
+        res.json(updatedPost);
+    });
+
+});
+
+
 app.get('/post', async (req,res) => {
     res.json(
       await Post.find()
@@ -107,7 +171,6 @@ app.get('/post', async (req,res) => {
     const postDoc = await Post.findById(id).populate('author', ['username']);
     res.json(postDoc);
   })
-
 
 app.listen(8800, (req,res)=>{
     console.log("Database connected successfully!!!! ");
